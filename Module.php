7957db16d28519b91c881d49c5efb2e6ca2f7ff4
Marco Perone <m.perone@mvlabs.it>
2015-05-22 12:47:47 +0200

    move Module.php

--- Module.php
+++ Module.php
@@ -1,26 +1,3 @@
 <?php
 
-namespace SharengoCore;
-
-class Module
-{
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    /**
-     * Returns autoloader configuration
-     * @return multitype:multitype:multitype:string
-     */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-}
+require_once __DIR__.'/src/SharengoCore/Module.php';
