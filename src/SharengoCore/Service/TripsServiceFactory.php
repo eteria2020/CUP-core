0e86f41f74c735dd99df5227e352bfad2cc5543d
Marco Perone <m.perone@mvlabs.it>
2015-06-10 15:25:46 +0200

    refactoring to use decorators on datatable service

--- src/SharengoCore/Service/TripsServiceFactory.php
+++ src/SharengoCore/Service/TripsServiceFactory.php
@@ -13,6 +13,11 @@ class TripsServiceFactory implements FactoryInterface
         $entityManager = $serviceLocator->get('doctrine.entitymanager.orm_default');
         $tripRepository = $entityManager->getRepository('\SharengoCore\Entity\Trips');
         $I_datatableService = $serviceLocator->get('SharengoCore\Service\DatatableService');
+        $I_datatableService = new DatatableDecorators\DatatableCarJoinService(
+            new DatatableDecorators\DatatableCustomerJoinService(
+                $I_datatableService
+            )
+        );
 
         return new TripsService($tripRepository, $I_datatableService);
     }
