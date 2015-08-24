96eeb4e1b78af2ebff6bae5be31fde88c4660927
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-08-24 18:44:10 +0200

    Added function to check if point is in polygon

--- src/SharengoCore/Service/LocationService.php
+++ src/SharengoCore/Service/LocationService.php
@@ -19,4 +19,26 @@ class LocationService
             return $jsondata['results'][0]['formatted_address'];
         }
     }
+
+    /**
+     * @param long[] $polyX coordinates of longitude
+     * @param long[] $polyY coordinates of latitude
+     * @param long $x
+     * @param long $y
+     */
+    public function IsointInPolygon($polyX,$polyY,$x,$y) {
+        $j = count($polyY) - 1 ;
+        $oddNodes = 0;
+
+        for ($i=0; $i<$polySides; $i++) {
+            if ($polyY[$i] < $y && $polyY[$j] >= $y || $polyY[$j] < $y && $polyY[$i] >= $y) {
+                if ($polyX[$i] + ($y - $polyY[$i]) / ($polyY[$j] - $polyY[$i]) * ($polyX[$j] - $polyX[$i]) < $x) {
+                    $oddNodes = !$oddNodes;
+                }
+            }
+            $j = $i;
+        }
+
+        return $oddNodes;
+    }
 }
