b4cf621d570b3ce9c6396acecacc60e9f94a6224
Marco Perone <m.perone@mvlabs.it>
2015-10-01 16:11:48 +0200

    fleet service

---
+++ src/SharengoCore/Entity/Queries/AllFleets.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SharengoCore\Entity\Queries;
+
+final class AllFleets extends Query
+{
+    protected function dql()
+    {
+        return 'SELECT f FROM \SharengoCore\Entity\Fleet f';
+    }
+}
---
+++ src/SharengoCore/Entity/Queries/FleetById.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SharengoCore\Entity\Queries;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+class FleetById extends Query
+{
+    /**
+     * @param int
+     */
+    private $fleetId;
+
+    public function __construct(
+        $fleetId,
+        EntityManagerInterface $em
+    ) {
+        $this->fleetId = $fleetId;
+
+        parent::__construct($em, 'getOneOrNullResult');
+    }
+
+    protected function dql()
+    {
+        return 'SELECT f FROM \SharengoCore\Entity\Fleet f '.
+            'WHERE f.id = :fleetId';
+    }
+
+    protected function params()
+    {
+        return [
+            'fleetId' => $this->fleetId
+        ];
+    }
+}
