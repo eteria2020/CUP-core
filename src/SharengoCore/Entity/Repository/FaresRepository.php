d9c444f311785c6193834743c335900a0b8abdfe
xantrix <x4ntrix@gmail.com>
2015-11-27 19:39:47 +0100

    Gestione tariffe

--- src/SharengoCore/Entity/Repository/FaresRepository.php
+++ src/SharengoCore/Entity/Repository/FaresRepository.php
@@ -2,12 +2,16 @@
 
 namespace SharengoCore\Entity\Repository;
 
-class FaresRepository extends \Doctrine\ORM\EntityRepository
+use Doctrine\ORM\EntityRepository;
+
+class FaresRepository extends EntityRepository
 {
     public function findOne()
     {
         return $this->createQueryBuilder('f')
             ->select()
+            ->orderBy('f.id', 'DESC')
+            ->setMaxResults(1)
             ->getQuery()
             ->getSingleResult();
     }
