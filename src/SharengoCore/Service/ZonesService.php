a33c27421cf39372b9e3f3e31c7def78666cc32e
Stefano Valle <s.valle@mvassociati.it>
2015-11-30 18:15:00 +0100

    List zone groups

--- src/SharengoCore/Service/ZonesService.php
+++ src/SharengoCore/Service/ZonesService.php
@@ -25,6 +25,7 @@ class ZonesService
         $this->entityManager = $entityManager;
         $this->zoneRepository = $this->entityManager->getRepository('\SharengoCore\Entity\Zone');
         $this->zoneAlarmsRepository = $this->entityManager->getRepository('\SharengoCore\Entity\ZoneAlarms');
+        $this->zoneGroupsRepository = $this->entityManager->getRepository('\SharengoCore\Entity\ZoneGroups');
     }
 
     /**
@@ -38,6 +39,20 @@ class ZonesService
     public function getListZonesAlarms()
     {
         return $this->zoneAlarmsRepository->findAll();
+    }
+
+    public function getListZonesGroups() {
+        $zoneGroups = $this->zoneGroupsRepository->findAll();
+
+        $zoneList = [];
+        foreach($zoneGroups as $zoneGroup) {
+            foreach($zoneGroup->getZonesList() as $zoneId) {
+                $zoneList[] = $this->zoneRepository->find($zoneId)->getName();
+            }
+            $zoneGroup->setZonesListText(implode(', ', $zoneList));
+        }
+
+        return $zoneGroups;
     }
 
 }
