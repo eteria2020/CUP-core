118ddc31f9481b8fdb094acb20010b7dc8222f6b
Flavio Del Bianco <info@flaviodelbianco.com>
2016-03-11 10:07:37 +0100

    Fix EventsRepository

--- src/SharengoCore/Document/Repository/EventsRepository.php
+++ src/SharengoCore/Document/Repository/EventsRepository.php
@@ -10,13 +10,17 @@ class EventsRepository extends DocumentRepository
     {
         $events = $this->findBy([ "trip" => (string)$trip->getId()], array("eventTime", "asc"));
         $is_same_trip = true;
+
+        if (!$events) {
+            return array();
+        }
         $startTime = $events[0]->getEventTime();
         $plate = $trip->getCar()->getPlate();
 
         while ($is_same_trip) {
             $event = $this->_getPreviousEvent($plate, $startTime);
 
-            if ($event->getTrip() == '0') {
+            if ($event && $event->getTrip() == '0') {
                 array_unshift($events, $event);
                 $startTime = $event->getEventTime();
             } else {
