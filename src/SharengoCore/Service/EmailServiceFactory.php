5ef7bb54ecce57c6f6f1eefa82531670e58521d2
Andrea Cadorin <cadorin89@gmail.com>
2016-03-14 14:18:36 +0100

    add mail dump to file while in development.

--- src/SharengoCore/Service/EmailServiceFactory.php
+++ src/SharengoCore/Service/EmailServiceFactory.php
@@ -2,17 +2,30 @@
 
 namespace SharengoCore\Service;
 
+use Zend\Mail\Transport\File;
+use Zend\Mail\Transport\FileOptions;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
-use Zend\Mail\Transport\Sendmail;
 
 class EmailServiceFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $emailTransport = new Sendmail();
-        $emailSettings = $serviceLocator->get('Configuration')['emailSettings'];
+        $config = $serviceLocator->get('Configuration');
+        $transportConfig = $config['emailTransport'];
+        $emailSettings = $config['emailSettings'];
 
+        $emailTransport = new $transportConfig['type'];
+
+        if ($emailTransport instanceof File) {
+            $options   = new FileOptions(array(
+                'path' => $transportConfig['filePath'],
+                'callback' => function (File $transport) {
+                    return 'Message_' . microtime(true) . '_' . mt_rand() . '.txt';
+                },
+            ));
+            $emailTransport->setOptions($options);
+        }
         return new EmailService($emailTransport, $emailSettings);
     }
 }
