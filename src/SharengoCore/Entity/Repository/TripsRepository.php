047a3678e396ce4c0c01b5862c16a7f41e8067ae
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2017-07-11 16:30:21 +0200

    add carFilter for nouse in free fares

--- src/SharengoCore/Entity/Repository/TripsRepository.php
+++ src/SharengoCore/Entity/Repository/TripsRepository.php
@@ -404,6 +404,7 @@ class TripsRepository extends \Doctrine\ORM\EntityRepository
         FROM \SharengoCore\Entity\Trips t
         WHERE t.car = :plate 
         AND t.timestampBeginning < (SELECT tr.timestampBeginning FROM \SharengoCore\Entity\Trips tr WHERE tr.id = :tripId) 
+        AND t.payable = true 
         AND t.customer NOT IN (SELECT c.id FROM \SharengoCore\Entity\Customers c WHERE c.maintainer = TRUE) 
         ORDER BY t.timestampBeginning DESC";
 
