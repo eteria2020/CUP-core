04472fa9ddd521cd3c51d1b304fab6099528fb14
Enrico Taddei <tommaso.garavaglia@cdesign-group.com>
2018-02-07 10:11:57 +0100

    sosallarmstep3biscore-180207-csd-1809: return corret json

--- src/SharengoCore/Service/NotificationsService.php
+++ src/SharengoCore/Service/NotificationsService.php
@@ -10,35 +10,38 @@ use Doctrine\ORM\EntityManager;
 use DateTime;
 use Zend\Authentication\AuthenticationService as UserService;
 
-class NotificationsService
-{
+class NotificationsService {
+
     private $entityManager;
-    
+
     /**
      * @var UserService
      */
     private $userService;
+    
+    /**
+     * @var CustomersService
+     */
+    private $customerService;
 
     /**
      * @param EntityManager $entityManager
      * @param NotificationsRepository $notificationsRepository
      * @param DatatableServiceInterface $datatableService
      * @param UserService $userService
+     * @param CustomersService $customerService
      */
     public function __construct(
-        EntityManager $entityManager,
-        NotificationsRepository $notificationsRepository,
-        DatatableServiceInterface $datatableService,
-        UserService $userService
+    EntityManager $entityManager, NotificationsRepository $notificationsRepository, DatatableServiceInterface $datatableService, UserService $userService, CustomersService $customerService
     ) {
         $this->entityManager = $entityManager;
         $this->notificationsRepository = $notificationsRepository;
         $this->datatableService = $datatableService;
         $this->userService = $userService;
+        $this->customerService = $customerService;
     }
 
-    public function getTotalNotifications()
-    {
+    public function getTotalNotifications() {
         return $this->notificationsRepository->getTotalNotifications();
     }
 
@@ -46,13 +49,11 @@ class NotificationsService
      * @param integer $id
      * @return Notifications
      */
-    public function getNotificationById($id)
-    {
+    public function getNotificationById($id) {
         return $this->notificationsRepository->findOneById($id);
     }
 
-    public function getDataDataTable(array $filters = [], $count = false)
-    {
+    public function getDataDataTable(array $filters = [], $count = false) {
         $notifications = $this->datatableService->getData('Notifications', $filters, $count);
 
         if ($count) {
@@ -63,26 +64,22 @@ class NotificationsService
             return [
                 'e' => [
                     'id' => $notifications->getId(),
-                    'subject' => $notifications->getSubject(),
                     'submitDate' =>
-                        ($notifications->getSubmitDate() instanceof DateTime) ?
-                        $notifications->getSubmitDate()->getTimestamp() : null,
+                    ($notifications->getSubmitDate() instanceof DateTime) ?
+                    $notifications->getSubmitDate()->getTimestamp() : null,
                     'sentDate' =>
-                        ($notifications->getSentDate() instanceof DateTime) ?
-                        $notifications->getSentDate()->getTimestamp() : null,
+                    ($notifications->getSentDate() instanceof DateTime) ?
+                    $notifications->getSentDate()->getTimestamp() : null,
                     'acknowledgeDate' =>
-                        ($notifications->getAcknowledgeDate() instanceof DateTime) ?
-                        $notifications->getAcknowledgeDate()->getTimestamp() : null,
+                    ($notifications->getAcknowledgeDate() instanceof DateTime) ?
+                    $notifications->getAcknowledgeDate()->getTimestamp() : null,
                     'webuser' => $notifications->getWebuser()
-                    
                 ],
-                'nc' => [
-                    'name' => $notifications->getCategoryName(),
+                't' => [
+                    'carPlate' => $notifications->getMeta()['car_plate'],
+                    'tripId' => $notifications->getMeta()['trip_id']
                 ],
-                'np' => [
-                    'name' => is_null($notifications->getProtocolName()) ? $notifications->getCategoryDefaultProtocolName() : $notifications->getProtocolName(),
-                ],
-                'button' => $notifications->getId(),
+                'c' => $this->getCustomer($this->customerService->findById($notifications->getMeta()['customer_id']))
             ];
         }, $notifications);
     }
@@ -94,21 +91,36 @@ class NotificationsService
      * @param Notifications $notification
      * @param DateTime $acknolageDate
      */
-    public function acknowledge(Notifications $notification, DateTime $acknolageDate)
-    {
+    public function acknowledge(Notifications $notification, DateTime $acknolageDate) {
         $notification->setAcknowledgeDate($acknolageDate);
 
         // persist and flush notification
         $this->entityManager->persist($notification);
         $this->entityManager->flush();
     }
-    
-    public function webuser(Notifications $notification)
-    {
+
+    public function webuser(Notifications $notification) {
         $notification->setWebuser($this->userService->getIdentity());
 
         // persist and flush notification
         $this->entityManager->persist($notification);
         $this->entityManager->flush();
     }
+
+    private function getCustomer($customer) {
+        if (isset($customer))
+            return [
+                'id' => $customer->getId(),
+                'nameSurname' => $customer->getSurname() . " " . $customer->getName(),
+                'mobile' => $customer->getMobile()
+            ];
+        else {
+            return [
+                'id' => null,
+                'nameSurname' => null,
+                'mobile' => null
+            ];
+        }
+    }
+
 }
