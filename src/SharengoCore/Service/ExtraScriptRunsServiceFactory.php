c1213433df48565b76a8755179b313e6a7648e04
Enrico Taddei <tommaso.garavaglia@cdesign-group.com>
2018-04-19 12:08:35 +0200

    retryextrascript-180419-csd1930: update

---
+++ src/SharengoCore/Service/ExtraScriptRunsServiceFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SharengoCore\Service;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class ExtraScriptRunsServiceFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $connection = $serviceLocator->get('doctrine.connection.orm_default');
+
+        return new PaymentScriptRunsService($connection);
+    }
+}
