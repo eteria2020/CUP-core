a4248fb9aa42b8c20f12cc81ef9a3f73883aa80c
Lorenzo Mancini <lorenzo.mancini@sharengo.eu>
2019-01-25 17:35:48 +0100

    add crawler service

---
+++ src/SharengoCore/Service/CrawlerService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace SharengoCore\Service;
+
+class CrawlerService
+{
+    private function apiRequest($id)
+    {
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://manage.sharengo.it/crawler/api.php?id=" . $id,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 10,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_POSTFIELDS => "",
+                CURLOPT_HTTPHEADER => array(
+                    "cache-control: no-cache"
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            if ($err) {
+                return null;
+            } else {
+                $response = json_decode($response, true);
+                return $response;
+            }
+        } catch(\Exception $e){
+            return null;
+        }
+    }
+
+    public function isValidUser($id){
+        $response = $this->apiRequest($id);
+
+        if (is_array($response)){
+            return (isset($response["status"]) && $response["status"] == 200);
+        } else {
+            return false;
+        }
+    }
+
+    public function getCustomerInformation($id){
+        
+        return $this->apiRequest($id);
+
+    }
+
+
+}
