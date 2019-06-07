518f06eafc0b213e397743c9a924fe0f45b5c6ef
Enrico Taddei <enrico.taddei@sharengo.eu>
2019-06-07 15:24:00 +0200

    Retry wrong payments time 190607 add no lock core

--- src/SharengoCore/Service/ProcessExtraService.php
+++ src/SharengoCore/Service/ProcessExtraService.php
@@ -2,6 +2,7 @@
 
 namespace SharengoCore\Service;
 
+use SharengoCore\Entity\ExtraPayments;
 use SharengoCore\Listener\PaymentEmailListener;
 use SharengoCore\Listener\NotifyCustomerPayListener;
 use SharengoCore\Service\TripPaymentsService;
@@ -66,6 +67,20 @@ class ProcessExtraService
      */
     private  $customersService;
 
+    /**
+     * ProcessExtraService constructor.
+     *
+     * @param EventManager $eventManager
+     * @param LoggerInterface $logger
+     * @param PaymentEmailListener $paymentEmailListener
+     * @param NotifyCustomerPayListener $notifyCustomerPayListener
+     * @param PaymentsService $paymentsService
+     * @param \SharengoCore\Service\TripPaymentsService $tripPaymentsService
+     * @param \SharengoCore\Service\ExtraPaymentsService $extraPaymentsService
+     * @param \SharengoCore\Service\CustomerDeactivationService $customerDeactivationService
+     * @param \SharengoCore\Service\UsersService $usersService
+     * @param \SharengoCore\Service\CustomersService $customersService
+     */
     public function __construct(
         EventManager $eventManager,
         LoggerInterface $logger,
@@ -90,6 +105,12 @@ class ProcessExtraService
         $this->customersService = $customersService;
     }
 
+    /**
+     * @param $extraPayments
+     * @param bool $avoidEmails
+     * @param bool $avoidCartasi
+     * @param bool $avoidPersistance
+     */
     public function processPayments(
         $extraPayments,
         $avoidEmails = true,
@@ -101,21 +122,31 @@ class ProcessExtraService
 
         foreach ($extraPayments as $extraPayment) {
             try {
-                $this->logger->log( date_create()->format('H:i:s').";INF;processPayments;extraPayment;".$extraPayment->getId() . ";customer;".$extraPayment->getCustomer()->getId()."\n");
+
                 $this->paymentsService->tryPaymentExtra(
                     $extraPayment,
                     $avoidEmails,
                     $avoidCartasi,
                     $avoidPersistance
                 );
+
+                $this->logger->log(sprintf("%s;INF;processPayments;c;%s;e;%s;%s;%s;%s\n",
+                    date_create()->format('y-m-d H:i:s'),
+                    $extraPayment->getCustomer()->getId(),
+                    $extraPayment->getId(),
+                    $extraPayment->getPaymentType(),
+                    $extraPayment->getAmount(),
+                    $extraPayment->getStatus()
+                ));
+
             } catch(\Doctrine\ORM\ORMException $de){
-                $this->logger->log( date_create()->format('H:i:s').";ERR;processPayments;doctrine exception;ExtraPayment->getId;".$extraPayment->getId() . "\n");
+                $this->logger->log( date_create()->format('y-m-d H:i:s').";ERR;processPayments;doctrine exception;ExtraPayment->getId;".$extraPayment->getId() . "\n");
                 $this->logger->log($de->getMessage() . " " . $de->getFile() . " line " . $de->getLine() . "\n");
                 $this->logger->log($de->getTraceAsString(). "\n");
                 // \Doctrine\Common\Util\Debug::dump($tripPayment);
                 break;
             } catch (\Exception $e) {
-                $this->logger->log( date_create()->format('H:i:s').";ERR;processPayments;general exception;ExtraPayment->getId;".$extraPayment->getId() . "\n");
+                $this->logger->log( date_create()->format('y-m-d H:i:s').";ERR;processPayments;general exception;ExtraPayment->getId;".$extraPayment->getId() . "\n");
                 $this->logger->log($e->getMessage() . " " . $e->getFile() . " line " . $e->getLine() . "\n");
                 $this->logger->log($e->getTraceAsString(). "\n");
                 // \Doctrine\Common\Util\Debug::dump($tripPayment);
@@ -132,6 +163,9 @@ class ProcessExtraService
         ]);*/
     }
 
+    /**
+     * @param bool $avoidEmails
+     */
     public function processPaymentsCompleted($avoidEmails = true){
 
         $this->eventManager->getSharedManager()->attachAggregate($this->paymentEmailListener);
@@ -143,12 +177,18 @@ class ProcessExtraService
 
     }
 
-    /*
+    /**
      * After re-processing wrong payment we try to enabled the customer that was disabled.
      * To enable a customer we do:
      * - put true the enabled record
      * - put true the able_payment record
      * - set end_ts into customer_deactivations table
+     *
+     * @param $extraPayments
+     * @param bool $avoidEmails
+     * @param bool $avoidCartasi
+     * @param bool $avoidPersistance
+     * @param null $timestampEndParam
      */
    public function processCustomersDisabledAfterReProcess(
         $extraPayments,
@@ -166,20 +206,23 @@ class ProcessExtraService
             }
         }
 
-        $this->logger->log(date_create()->format('H:i:s').";INF;processCustomersDisabledAfterReProcess;count(arrayOfCustomers);" . count($arrayOfCustomers) . "\n");
+        $this->logger->log(date_create()->format('y-m-d H:i:s').";INF;processCustomersDisabledAfterReProcess;count(arrayOfCustomers);" . count($arrayOfCustomers) . "\n");
         foreach ($arrayOfCustomers as $customer) {
             
             if(count($this->extraPaymentsService->getExtraPaymentsWrong($customer, $timestampEndParam))===0){
-                $this->logger->log(date_create()->format('H:i:s').";INF;processCustomersDisabledAfterReProcess;" . $customer->getId() . ";enabled\n");
+                $this->logger->log(date_create()->format('y-m-d H:i:s').";INF;processCustomersDisabledAfterReProcess;" . $customer->getId() . ";enabled\n");
                 $webuser = $this->usersService->findUserById(12);
                 $this->customersService->enableCustomerPayment($customer);
                 $this->customerDeactivationService->reactivateCustomer($customer, $webuser, "customer enabled from retry wrong payments process", date_create());
             } else {
-                $this->logger->log(date_create()->format('H:i:s').";INF;processCustomersDisabledAfterReProcess;" . $customer->getId() . ";stay disabled\n");
+                $this->logger->log(date_create()->format('y-m-d H:i:s').";INF;processCustomersDisabledAfterReProcess;" . $customer->getId() . ";stay disabled\n");
             }
         }
     }
 
+    /**
+     * @param LoggerInterface $logger
+     */
     public function setLogger(LoggerInterface $logger)
     {
         $this->logger = $logger;
