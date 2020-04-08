b7910e7e37a6c904a050f17eb9e39f4e9d805a83
Federico Biagini <federico.biagini@sharengo.eu>
2020-04-08 17:17:43 +0200

    20200406 public nopaypacket

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.14] - 2020-04-08
+### Changed
+- The page dedicated to the purchase of packages in the user area has been modified 
+
 ## [1.0.13] - 2020-03-05
 
 ### Changed
--- src/SharengoCore/Service/BuyCustomerBonusPackage.php
+++ src/SharengoCore/Service/BuyCustomerBonusPackage.php
@@ -4,24 +4,18 @@ namespace SharengoCore\Service;
 
 use Cartasi\Service\CartasiCustomerPaymentsInterface;
 use Cartasi\Service\CartasiContractsService;
-
 use GPWebpay\Service\GPWebpayCustomerPayments;
-
 use Mollie\Service\MollieCustomerPayments;
-
 use Bankart\Service\BankartCustomerPayments;
-
 use SharengoCore\Entity\Customers;
 use SharengoCore\Entity\CustomersBonusPackages;
 use SharengoCore\Entity\CustomersPoints;
 use SharengoCore\Entity\BonusPackagePayment;
-
 use SharengoCore\Service\CustomersPointsService;
-
 use Doctrine\ORM\EntityManager;
 
-class BuyCustomerBonusPackage
-{
+class BuyCustomerBonusPackage {
+
     /**
      * @var EntityManager
      */
@@ -31,7 +25,7 @@ class BuyCustomerBonusPackage
      * @var CartasiCustomerPaymentsInterface
      */
     private $payments;
-    
+
     /**
      * @var CustomersPointsService
      */
@@ -58,13 +52,13 @@ class BuyCustomerBonusPackage
     private $bankartCustomerPayments;
 
     public function __construct(
-        EntityManager $entityManager,
-        CartasiCustomerPaymentsInterface $payments,
-        CustomersPointsService $customersPointsService,
-        CartasiContractsService $cartasiContractService,
-        $gpwebpayCustomerPayments,
-        $mollieCustomerPayments,
-        $bankartCustomerPayments
+            EntityManager $entityManager,
+            CartasiCustomerPaymentsInterface $payments,
+            CustomersPointsService $customersPointsService,
+            CartasiContractsService $cartasiContractService,
+            $gpwebpayCustomerPayments,
+            $mollieCustomerPayments,
+            $bankartCustomerPayments
     ) {
         $this->entityManager = $entityManager;
         $this->payments = $payments;
@@ -78,60 +72,72 @@ class BuyCustomerBonusPackage
     /**
      * @param Customers $customer
      * @param CustomersBonusPackages $package
+     * @param bool $no_pay MYSHARENGO
      * @return bool whether the operation concluded positively
      */
     public function __invoke(
-        Customers $customer,
-        CustomersBonusPackages $package
+            Customers $customer,
+            CustomersBonusPackages $package,
+            $no_pay = false
     ) {
         $this->entityManager->beginTransaction();
 
         try {
-            if($package->getType() === "Pacchetto" && ($package->getCode()=="WOMEN" && (date_create() >= date_create('2018-03-08 06:00:00') && date_create() <= date_create('2018-03-09 04:55:00')))){
+            if ($package->getType() === "Pacchetto" && ($package->getCode() == "WOMEN" && (date_create() >= date_create('2018-03-08 06:00:00') && date_create() <= date_create('2018-03-09 04:55:00')))) {
                 $bonus = $package->generateCustomerWomenBonus($customer);
                 $this->entityManager->persist($bonus);
                 $this->entityManager->flush();
-            }
-            elseif($package->getType() === "Pacchetto"){
+            } elseif ($package->getType() === "Pacchetto") {
 
-                $contract = $this->cartasiContractService->getCartasiContract($customer);
-
-                if(!is_null($contract->getPartner())){
-                    if($contract->getPartner()->getCode() == "gpwebpay") {
-                        $cartasiResponse = $this->gpwebpayCustomerPayments->sendPaymentRequest($customer, $package->getCost());
-                    } elseif ($contract->getPartner()->getCode() == "mollie") {
-                        $cartasiResponse = $this->mollieCustomerPayments->sendPaymentRequest($customer, $package->getCost());
-                    } elseif ($contract->getPartner()->getCode() == "bankart") {
-                        $cartasiResponse = $this->bankartCustomerPayments->sendPaymentRequest($customer, $package->getCost());
-                    }
-                } else {
-                    $cartasiResponse = $this->payments->sendPaymentRequest($customer, $package->getCost());
-                }
-
-                if ($cartasiResponse->getCompletedCorrectly()) {
-                    if ($package->getCode()=="WOMEN") { //check if package women
+                if ($no_pay) { // MYSHARENGO
+                    if ($package->getCode() == "WOMEN") { //check if package women
                         $bonus = $package->generateCustomerWomenBonus($customer);
                     } else {
                         $bonus = $package->generateCustomerBonus($customer);
                     }
-                    //$bonus = $package->generateCustomerBonus($customer);
-                    $bonusPayment = new BonusPackagePayment(
-                        $customer,
-                        $bonus,
-                        $package,
-                        $cartasiResponse->getTransaction()
-                    );
-
+                                        
                     $this->entityManager->persist($bonus);
-                    $this->entityManager->persist($bonusPayment);
+
                     $this->entityManager->flush();
                 } else {
-                    return false;
+                    $contract = $this->cartasiContractService->getCartasiContract($customer);
+
+                    if (!is_null($contract->getPartner())) {
+                        if ($contract->getPartner()->getCode() == "gpwebpay") {
+                            $cartasiResponse = $this->gpwebpayCustomerPayments->sendPaymentRequest($customer, $package->getCost());
+                        } elseif ($contract->getPartner()->getCode() == "mollie") {
+                            $cartasiResponse = $this->mollieCustomerPayments->sendPaymentRequest($customer, $package->getCost());
+                        } elseif ($contract->getPartner()->getCode() == "bankart") {
+                            $cartasiResponse = $this->bankartCustomerPayments->sendPaymentRequest($customer, $package->getCost());
+                        }
+                    } else {
+                        $cartasiResponse = $this->payments->sendPaymentRequest($customer, $package->getCost());
+                    }
+
+                    if ($cartasiResponse->getCompletedCorrectly()) {
+                        if ($package->getCode() == "WOMEN") { //check if package women
+                            $bonus = $package->generateCustomerWomenBonus($customer);
+                        } else {
+                            $bonus = $package->generateCustomerBonus($customer);
+                        }
+                        //$bonus = $package->generateCustomerBonus($customer);
+                        $bonusPayment = new BonusPackagePayment(
+                                $customer,
+                                $bonus,
+                                $package,
+                                $cartasiResponse->getTransaction()
+                        );
+
+                        $this->entityManager->persist($bonus);
+                        $this->entityManager->persist($bonusPayment);
+                        $this->entityManager->flush();
+                    } else {
+                        return false;
+                    }
                 }
             } else { //$package->getType() === "PacchettoPunti"
+                if ($this->customersPointsService->getTotalPoints($customer->getId()) >= $package->getCost()) {
 
-                if($this->customersPointsService->getTotalPoints($customer->getId()) >= $package->getCost()){
-                    
                     $customersPoints = new CustomersPoints();
                     $customersPoints = $this->customersPointsService->setCustomerPointPackage($customersPoints, $customer, $package);
                     $bonus = $package->generateCustomerBonus($customer);
@@ -139,8 +145,7 @@ class BuyCustomerBonusPackage
                     $this->entityManager->persist($bonus);
                     $this->entityManager->persist($customersPoints);
                     $this->entityManager->flush();
-                    
-                }else{
+                } else {
                     return false;
                 }
             }
@@ -153,4 +158,5 @@ class BuyCustomerBonusPackage
 
         return true;
     }
+
 }
